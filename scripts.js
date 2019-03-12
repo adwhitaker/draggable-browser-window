6141def073f8c2af3c8ebb3fa6f6a707484ea452
Alexander Whitaker <whitaker.alexander@gmail.com>
2019-03-11 20:57:59 -0500

    drag box across screen

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const element = document.querySelector('.browser');
+
+let state = {
+    moving: false,
+    zIndex: 1,
+    element: element,
+    width: 0,
+    height: 0
+}
+
+element.addEventListener('mousedown', (event) => drag(event, state))
+element.addEventListener('mouseup', (event) => state.moving = false)
+
+const drag = (event, state) => {
+    state.moving = true
+
+    state.width = state.element.offsetWidth / 2;
+    state.height = state.element.offsetHeight / 2;
+
+    document.addEventListener('mousemove', function (e) {
+        if (state.moving) {
+            const x = e.clientX - state.width;
+            const y = e.clientY - state.height;
+
+            const position = `left:${x}px;top:${y}px;`;
+
+            state.element.setAttribute('style', position);
+        };
+    });
+}
