b0ed80cf9aa8da19f3bfa2f7836b5f09a638dd3f
Alexander Whitaker <whitaker.alexander@gmail.com>
2019-03-13 13:01:18 -0500

    refactor moving mouse

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-const element = document.querySelector('.browser');
 
 let state = {
     moving: false,
@@ -7,23 +6,26 @@ let state = {
     element
 }
 
-element.addEventListener('mousedown', (event) => drag(state))
-element.addEventListener('mouseup', (event) => state.moving = false)
-
 const drag = (state) => {
     state.moving = true
 
     state.width = state.element.offsetWidth / 2;
     state.height = state.element.offsetHeight / 2;
-    
-    document.addEventListener('mousemove', (event) => {
-        if (state.moving) {
-            const x = event.clientX - state.width
-            const y = event.clientY - state.height
-
-            const position = `left:${x}px;top:${y}px;`
-
-            state.element.setAttribute('style', position)
-        }
-    })
 }
+
+const move = (event, state) => {
+    if (state.moving) {
+        const x = event.clientX - state.width
+        const y = event.clientY - state.height
+
+        const position = `left:${x}px;top:${y}px;`
+
+        state.element.setAttribute('style', position)
+    }
+}
+
+const element = document.querySelector('.browser');
+
+element.addEventListener('mousedown', (event) => drag(state))
+element.addEventListener('mouseup', (event) => state.moving = false)
+element.addEventListener('mousemove', (event) => move(event, state))
